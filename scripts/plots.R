f2bf0ac49c6f6efa70d89690d369fed834d28db1
merle.behr <merle.behr@googlemail.com>
2020-04-15 15:26:20 -0700

    plots

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,8 +1,8 @@
 
 m <- 2
 al <- 0:1
-sigma <- 0.1
-N <- 1000
+sigma <- 0.01
+N <- 2
 file_name <- paste0("../results/mse_m_",m, "_sigma_", 100 * sigma, "_al_", max(al), "_N_", N, ".Rdata")
 if(file.exists(file_name)){
   load(file = file_name)
@@ -15,10 +15,10 @@ mseNm <- (matrix(sapply(mseN, mean), nrow = length(nN)))
 
 par(mfrow = c(1,1))
 ind <- 1:length(nN)
-plot(nN[ind], mseNm[ind,1], type = "b", ylim = range(mseNm[ind,])) 
+plot(nN[ind], mseNm[ind,1], type = "b", ylim = range(mseNm[ind,]), xlab = "n", ylab = "MSE") 
 for(i in 2:ncol(mseNm)){
   lines(nN[ind], (mseNm[ind,i]), type = "b") 
-  y <- mseN[ind,i]
+  y <- mseNm[ind,i]
   x <- 1/nN[ind]
   fit <- lm(y ~ x)
   lines(nN[ind], fit$fitted.values, col = "red", lwd = 2)
