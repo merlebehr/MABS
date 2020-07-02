b3389aadfd442860446130c3c876b2a36fc45b87
merle.behr <merle.behr@googlemail.com>
2020-07-02 09:34:47 -0700

    minor changes

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,7 +1,7 @@
 
 m <- 2
-al <- 0:2
-sigma <- 1
+al <- 0:1
+sigma <- 1.5
 plot_fit <- T
 
 file_name <- file_name <- paste0("mse_m_",m, "_sigma_", 100 * sigma, "_al_", max(al))
@@ -37,7 +37,15 @@ limY <- range(mseNm[,])
 colM <- c("black", "blue", "orange", "darkgreen", "red", "brown", "purple")
 plot(nN, mseNm[,1], type = "l", ylim = limY, xlab = "n", ylab = expression(paste("||",hat(omega) - omega,"||"^2,""["Inf,2"]/M)), col = colM[1], lwd = 2) 
 
-title(main = paste0("m = ",m, ", A = 0,...,", max(al), ", sigma = ",sigma), outer = T) #", number of MC runs = ", mc_runs
+if(max(al) == 1){
+  al_name = "{0,1}"
+}else if (max(al) == 2){
+  al_name = "{0,1,2}"
+}else{
+  al_name = paste0("{", min(al), "...", max(al), "}")
+}
+
+title(main = paste0("m = ",m, ", alphabet = ", al_name, ", sigma = ",sigma), outer = T) #", number of MC runs = ", mc_runs
 
 if(plot_fit){
   y <- mseNm[ind,1]
@@ -60,7 +68,17 @@ for(i in 2:ncol(mseNm)){
   #        x.intersp = 0.8, y.intersp = 0.8)
 }
 
-plot(nN[ind], accANm[ind,1], type = "l", ylim = c(0,1), xlab = "n",ylab = expression(paste('mean( ', F ==  hat(F), ' )')), col = colM[1], lwd = 2) 
+name_ylab <- expression(paste('mean( ', F ==  hat(F), ' )'))
+
+expression(paste("||",hat(omega) - omega,"||"^2,""["Inf,2"]/M))
+
+#sub_expression <- expression(paste())
+#ylab_name <- expression(paste(Sigma, "1", ""[ "F",""["i,j"], " = ", hat(F),""["i,j"]], "/(nM)"))
+
+ylab_name <- expression("( " * Sigma * ""["i,j"] * "1" * ""[ "{ F" * ""["i,j"] * " = " * hat(F) * ""["i,j"] * "}" ] * " ) / (nM)")
+
+
+plot(nN[ind], accANm[ind,1], type = "l", ylim = c(0,1), xlab = "n",ylab = ylab_name, col = colM[1], lwd = 2) 
 for(i in 2:ncol(mseNm)){
   lines(nN[ind], (accANm[ind,i]), type = "l", col = colM[i], lwd = 2) 
   legend(x = min(nN[ind]) +  0.45 *( max(nN[ind]) - min(nN[ind])), y = 0.5, 
