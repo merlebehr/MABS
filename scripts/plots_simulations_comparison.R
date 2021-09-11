ccdbcd617c857086dbb84d5e1288d470dc800016
merlebehr <merle.behr@googlemail.com>
2021-09-11 16:25:16 +0200

    simulations for method comparison, several edits

--- scripts/plots_simulations_comparison.R
+++ scripts/plots_simulations_comparison.R
@@ -1,8 +1,7 @@
 
 m <- 2
-al <- 0:2
-sigma <- 0.01
-plot_fit <- T
+al <- 1
+sigma <- 1
 
 file_name <- file_name <- paste0("comparison_m_",m, "_sigma_", 100 * sigma, "_al_", max(al))
 file_path <- paste0("../results/")
@@ -13,12 +12,15 @@ if(length(file_name_all) > 0){
   mseN_c <- mseN
   accAN_c <- accAN
   mseImN_c <- mseImN
+  runTmN_c <- runTmN
   
   mseN_nmf_c <- mseN_nmf
   accAN_nmf_c <- accAN_nmf
   mseImN_nmf_c <- mseImN_nmf
+  runTmN_nmf_c <- runTmN_nmf
   
   mseImN_em_c <- mseImN_em
+  runTmN_em_c <- runTmN_em
   
   
   if(length(file_name_all) > 1){
@@ -28,12 +30,18 @@ if(length(file_name_all) > 0){
         mseN_c <- mapply(c, mseN_c, mseN, SIMPLIFY=FALSE)
         accAN_c <- mapply(c, accAN_c, accAN, SIMPLIFY=FALSE)
         mseImN_c <- mapply(c, mseImN_c, mseImN, SIMPLIFY=FALSE)
+        runTN_c <- mapply(c, runTmN_c, runTmN, SIMPLIFY=FALSE)
+        
         
         mseN_nmf_c <- mapply(c, mseN_nmf_c, mseN_nmf, SIMPLIFY=FALSE)
         accAN_nmf_c <- mapply(c, accAN_nmf_c, accAN_nmf, SIMPLIFY=FALSE)
         mseImN_nmf_c <- mapply(c, mseImN_nmf_c, mseImN_nmf, SIMPLIFY=FALSE)
+        runTN_nmf_c <- mapply(c, runTmN_nmf_c, runTmN_nmf, SIMPLIFY=FALSE)
+        
         
         mseImN_em_c <- mapply(c, mseImN_em_c, mseImN_em, SIMPLIFY=FALSE)
+        runTN_em_c <- mapply(c, runTmN_em_c, runTmN_em, SIMPLIFY=FALSE)
+        
         
       }
     }
@@ -45,23 +53,31 @@ if(length(file_name_all) > 0){
 mc_runs <- length(mseN_c[[1]])
 print(paste("In total there are ", mc_runs, "MC runs."))
 
+indN_range = 8:length(nN)
+
 mseNm <- (matrix(sapply(mseN_c, mean), nrow = length(nN)))
 accANm <- (matrix(sapply(accAN_c, mean), nrow = length(nN)))
 mseImNm <- (matrix(sapply(mseImN_c, mean), nrow = length(nN)))
+runTNm <- (matrix(sapply(runTmN_c, mean), nrow = length(nN)))
+
 
 mseNm_nmf <- (matrix(sapply(mseN_nmf_c, mean), nrow = length(nN)))
 accANm_nmf <- (matrix(sapply(accAN_nmf_c, mean), nrow = length(nN)))
 mseImNm_nmf <- (matrix(sapply(mseImN_nmf_c, mean), nrow = length(nN)))
+runTNm_nmf <- (matrix(sapply(runTmN_nmf_c, mean), nrow = length(nN)))
 
-mseImNm_em <- (matrix(sapply(mseImN_em_c, mean), nrow = length(nN)))
+
+mseImNm_em <- (matrix(sapply(mseImN_em_c, mean, na.rm = TRUE), nrow = length(nN)))
+runTNm_em <- (matrix(sapply(runTmN_em_c, mean), nrow = length(nN)))
 
 
-pdf(file = paste0("../results/plots/plot_comparison_",file_name,".pdf"), width = 12, height = 7)
-par(mfrow = c(1,3), mar = c(5,5,1,1), oma = c(0,0,3,0), cex = 1.5)
-ind <- 1:length(nN)
-limY <- range(c(mseNm[,], mseNm_nmf))
+
+pdf(file = paste0("../results/plots/plot_comparison_",file_name,".pdf"), width = 12, height = 12)
+par(mfrow = c(2,2), mar = c(5,5,1,1), oma = c(0,0,3,0), cex = 1.5)
+
+limY <- range(c(mseNm[indN_range,], mseNm_nmf[indN_range,]))
 colM <- c("black", "blue", "orange", "darkgreen", "red", "brown", "purple")
-plot(nN, mseNm[,1], type = "l", ylim = limY, xlab = "n", 
+plot(nN[indN_range], mseNm[indN_range,1], type = "l", ylim = limY, xlab = "n", 
      ylab = expression(paste("||",hat(omega) - omega,"||"^2,""["Inf,2"]/M)), col = colM[1], lwd = 2) 
 lines(nN, mseNm_nmf[,1], col = colM[1], lwd = 2, lty = 2)
 
@@ -84,27 +100,25 @@ for(i in 2:ncol(mseNm)){
 
 
 #ylab_name <- expression("( " * Sigma * ""["i,j"] * "1" * ""[ "{ F" * ""["i,j"] * " = " * hat(F) * ""["i,j"] * "}" ] * " ) / (nM)")
-ylab_name <- expression(paste("||",hat(F) - F,"||"^2,""["Inf,2"]/M))
+ylab_name <- expression(paste("||",hat(F) - F,"||"^2,""["2,Inf"]/M))
+limY <- range(c(accANm[indN_range,], accANm_nmf[indN_range,]))
 
-plot(nN[ind], accANm[ind,1], type = "l", ylim = c(0,1), xlab = "n",ylab = ylab_name, col = colM[1], lwd = 2) 
-lines(nN[ind], accANm_nmf[ind,1], col = colM[1], lwd = 2, lty = 2) 
+plot(nN[indN_range], accANm[indN_range,1], type = "l", ylim = limY, xlab = "n",ylab = ylab_name, col = colM[1], lwd = 2) 
+lines(nN, accANm_nmf[,1], col = colM[1], lwd = 2, lty = 2) 
 
 for(i in 2:ncol(mseNm)){
-  lines(nN[ind], (accANm[ind,i]), type = "l", col = colM[i], lwd = 2) 
-  lines(nN[ind], (accANm_nmf[ind,i]), type = "l", col = colM[i], lwd = 2, lty = 2) 
-  legend(x = min(nN[ind]) +  0.45 *( max(nN[ind]) - min(nN[ind])), y = 0.5, 
-         legend = paste("M = ", MN), col = colM, lty = rep(1, length(MN)),
-         x.intersp = 0.8, y.intersp = 0.8)
+  lines(nN, (accANm[,i]), type = "l", col = colM[i], lwd = 2) 
+  lines(nN, (accANm_nmf[,i]), type = "l", col = colM[i], lwd = 2, lty = 2) 
   abline(h = 1, col = "gray")
 }
 
 
 
 
-limY <- range(c(mseImNm[,], mseImNm_nmf, mseImNm_em))
+limY <- range(c(mseImNm[indN_range,], mseImNm_nmf[indN_range,], mseImNm_em[indN_range,]), na.rm = T)
 colM <- c("black", "blue", "orange", "darkgreen", "red", "brown", "purple")
-plot(nN, mseImNm[,1], type = "l", ylim = limY, xlab = "n", 
-     ylab = expression(paste("||",hat(F) * hat(omega) - F * omega,"||"^2,""["Inf,2"]/(Mn))), col = colM[1], lwd = 2) 
+plot(nN[indN_range], mseImNm[indN_range,1], type = "l", ylim = limY, xlab = "n", 
+     ylab = expression(paste("||",hat(F) * hat(omega) - F * omega,"||"^2,""[""]/(Mn))), col = colM[1], lwd = 2) 
 lines(nN, mseImNm_nmf[,1], col = colM[1], lwd = 2, lty = 2)
 lines(nN, mseImNm_em[,1], col = colM[1], lwd = 2, lty = 3)
 
@@ -117,6 +131,25 @@ for(i in 2:ncol(mseNm)){
 }
 
 
+limY <- range(c(runTNm[indN_range,], runTNm_nmf[indN_range,], runTNm_em[indN_range[1:2], 1:2]), na.rm = T) #runTNm_em
+plot(nN[indN_range], runTNm[indN_range,1], type = "l", ylim = limY, xlab = "n", 
+     ylab = "runtime in seconds", col = colM[1], lwd = 2) 
+lines(nN, runTNm_nmf[,1], col = colM[1], lwd = 2, lty = 2)
+lines(nN, runTNm_em[,1], col = colM[1], lwd = 2, lty = 3)
+
+
+for(i in 2:ncol(runTNm)){
+  lines(nN, (runTNm[,i]), col = colM[i], lwd = 2) 
+  lines(nN, (runTNm_nmf[,i]), col = colM[i], lwd = 2, lty = 2) 
+  lines(nN, (runTNm_em[,i]), col = colM[i], lwd = 2, lty = 3) 
+  
+}
+
+legend(x = min(nN[indN_range]) +  0.45 *( max(nN[indN_range]) - min(nN[indN_range])), y = 0.8 * max(limY), 
+       legend = c(paste("M = ", MN), c("MABS", "NMF", "EM")), 
+       col = c(colM[1:length(MN)], rep("darkgray", 3)), 
+       lty = c(rep(1, length(MN)), 1:3),
+       x.intersp = 0.8, y.intersp = 0.8)
 
 dev.off()
 
--- scripts/simulations_methods_comparison.R
+++ scripts/simulations_methods_comparison.R
@@ -31,20 +31,24 @@ print(paste('m = ', m, 'sigma = ', sigma, 'al = 0:', max(al), 'N = ', N, 'seed =
 K <- length(al)^m
 Am <- as.matrix(expand.grid(rep(list(al), m)))
 
-MN <- c(10, 20, 100)
-nN <- c(50, 60, 70, 80, 90, seq(100, 500, 50), seq(600, 1000, 100))
+MN <- c(20, 50)
+nN <- c(50, 60, 70, 80, 90, seq(100, 500, 50), seq(600, 1000, 200), 1500, 2000)
 
 
 mseN <- vector('list', length(MN) * length(nN))
 accAN <- vector('list', length(MN) * length(nN))
 mseImN <- vector('list', length(MN) * length(nN))
+runTmN <- vector('list', length(MN) * length(nN))
+
 
 mseN_nmf <- vector('list', length(MN) * length(nN))
 accAN_nmf <- vector('list', length(MN) * length(nN))
 mseImN_nmf <- vector('list', length(MN) * length(nN))
+runTmN_nmf <- vector('list', length(MN) * length(nN))
+
 
 mseImN_em <- vector('list', length(MN) * length(nN))
-
+runTmN_em <- vector('list', length(MN) * length(nN))
 
 
 ptm <- proc.time()
@@ -61,12 +65,16 @@ for(l in 1:length(MN)){
     mse <- numeric(N) + 0
     accA <- numeric(N) + 0
     mseIm <- numeric(N) + 0
+    runT <- numeric(N) + 0
+    
     
     mse_nmf <- numeric(N) + 0
     accA_nmf <- numeric(N) + 0
     mseIm_nmf <- numeric(N) + 0
+    runT_nmf <- numeric(N) + 0
     
     mseIm_em <- numeric(N) + 0
+    runT_em <- numeric(N) + 0
     
     
     for(k in 1:N){
@@ -76,23 +84,30 @@ for(l in 1:length(MN)){
       
       y_pos = apply(y, c(1,2), function(x) pmin(1, pmax(10e-5, x)))
       
+      timeNow = proc.time()
       ans_nmf = nmf(y_pos, m)
       estOm_nmf = ans_nmf@fit@H
       estA_nmf = ans_nmf@fit@W
+      runT_nmf[k] = max(proc.time() - timeNow)
       
+      timeNow = proc.time()
       ans_em = try(mvnormalmixEM(y, k = length(al)^m, arbvar = FALSE), silent = T)
       if(class(ans_em) == "mixEM"){
         estIm_em = ans_em$mu[apply(ans_em$posterior, 1, function(x) which.max(x))]
         estIm_em = as.data.frame(do.call(rbind, estIm_em))
       }else{
-        estIm_em = NULL #t(matrix(rep(colMeans(y), nrow(y)), ncol(y)))
+        estIm_em = NA #t(matrix(rep(colMeans(y), nrow(y)), ncol(y)))
       }
+      runT_em[k] = max(proc.time() - timeNow)
       
       
+      timeNow = proc.time()
       estOm <- estOmega(y, m, al)
       valEst <- Am %*% estOm
       indEstA <- sapply(1:nrow(y), function(i) which.min(colSums( (y[i,] - t(valEst) )^2 ) ))
       estA <- Am[indEstA, ]
+      runT[k] = max(proc.time() - timeNow)
+      
       
       perm <- permn(1:m)
       
@@ -104,18 +119,18 @@ for(l in 1:length(MN)){
 
       for(i in 1:length(perm)){
         mse_perm[i] <- max(rowSums((estOm[perm[[i]],] - trOm)^2))/M
-        accA_perm[i] <- max(rowSums((estA[,perm[[i]]] - trA)^2))/n
+        accA_perm[i] <- max(colSums((estA[,perm[[i]]] - trA)^2))/n
         
         mse_nmf_perm[i] <- max(rowSums((estOm_nmf[perm[[i]],] - trOm)^2))/M
-        accA_nmf_perm[i] <- max(rowSums((estA_nmf[,perm[[i]]] - trA)^2))/n
+        accA_nmf_perm[i] <- max(colSums((estA_nmf[,perm[[i]]] - trA)^2))/n
         
       }
       mse[k] <- min(mse_perm)
-      accA[k] <- max(accA_perm)
+      accA[k] <- min(accA_perm)
       mseIm[k] <- sum((estA %*% estOm - trA %*% trOm)^2)/(M * n)
       
       mse_nmf[k] <- min(mse_nmf_perm)
-      accA_nmf[k] <- max(accA_nmf_perm)
+      accA_nmf[k] <- min(accA_nmf_perm)
       mseIm_nmf[k] <- sum((estA_nmf %*% estOm_nmf - trA %*% trOm)^2)/(M * n)
       
       mseIm_em[k] <- sum((estIm_em - trA %*% trOm)^2)/(M * n)
@@ -124,12 +139,17 @@ for(l in 1:length(MN)){
     mseN[[length(nN)*(l - 1) + j]] <- mse
     accAN[[length(nN)*(l - 1) + j]] <- accA
     mseImN[[length(nN)*(l - 1) + j]] <- mseIm
+    runTmN[[length(nN)*(l - 1) + j]] <- runT
+    
     
     mseN_nmf[[length(nN)*(l - 1) + j]] <- mse_nmf
     accAN_nmf[[length(nN)*(l - 1) + j]] <- accA_nmf
     mseImN_nmf[[length(nN)*(l - 1) + j]] <- mseIm_nmf
+    runTmN_nmf[[length(nN)*(l - 1) + j]] <- runT_nmf
+    
     
     mseImN_em[[length(nN)*(l - 1) + j]] <- mseIm_em
+    runTmN_em[[length(nN)*(l - 1) + j]] <- runT_em
     
     
   }
@@ -139,7 +159,8 @@ print(paste("total run time was", time[1], time[2], time[3]))
 
 save(file = paste0("../results/comparison_m_",m, "_sigma_", 100 * sigma, "_al_", max(al), "_N_", N, "_seed_", seed, ".Rdata"), 
      mseN, mseN_nmf,
-     accAN, accA_nmf,
-     mseImN, mseIm_nmf,mseIm_em,
+     accAN, accAN_nmf,
+     mseImN, mseImN_nmf,mseImN_em,
+     runTmN, runTmN_nmf, runTmN_em,
      MN, nN, m, sigma, al, N)
 
